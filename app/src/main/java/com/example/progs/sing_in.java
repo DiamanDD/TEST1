3b4020ca383a74ffd8fad3d72f4281bf23fc94ed
mashasi98 <85038575+mashasi98@users.noreply.github.com>
2022-01-19 19:17:44 +0300

    UI

---
+++ app/src/main/java/com/example/progs/sing_in.java
@@ -0,0 +1,13 @@
+package com.example.progs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class sing_in extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_in);
+    }
+}
