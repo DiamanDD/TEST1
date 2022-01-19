79e51f7600bc417aaf31e489fdcb4563f1e0066f
Krobelius <oshargorodskiy@list.ru>
2022-01-19 18:49:17 +0300

    check commit

--- app/src/main/java/com/example/progs/MainActivity.java
+++ app/src/main/java/com/example/progs/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.progs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+//a12313
 public class MainActivity extends AppCompatActivity {
 
     @Override
