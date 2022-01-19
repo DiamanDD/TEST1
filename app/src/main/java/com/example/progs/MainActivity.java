9c45ed73c0849bf6269d650aee4ce3b3bed3f799
mashasi98 <85038575+mashasi98@users.noreply.github.com>
2022-01-19 20:38:19 +0300

    btn

--- app/src/main/java/com/example/progs/MainActivity.java
+++ app/src/main/java/com/example/progs/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.progs;
 
+import android.service.autofill.Validator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-//a12313
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.Button;
 
+//a12313
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button inBtn,uythbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        if(view.getId()==R.id.button2){}
+
     }
 }
